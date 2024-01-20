7b40895463cf8dfb9d8dfeae38c967c38beaff7f
Guilherme Lucon Pinto <guilucon@gmail.com>
2024-01-20 02:25:18 -0300

    Projeto Blog - DIO

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
   {
     "id":"1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "description": "O Homem de Ferro 3D oferece uma experiência cinematográfica imersiva e eletrizante, elevando o icônico super-herói da Marvel a novos patamares visuais. Com efeitos tridimensionais impressionantes, os espectadores são transportados para o mundo tecnológico e cheio de ação de Tony Stark, onde suas armaduras high-tech ganham vida de maneira espetacular. Cada detalhe, desde os impressionantes trajes até os emocionantes combates, é aprimorado pela tecnologia 3D, proporcionando uma vivacidade e profundidade que cativam os fãs e tornam a jornada do Homem de Ferro ainda mais emocionante e envolvente.",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
     "id":"2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "description": "Explore novos mundos e mergulhe em histórias emocionantes com a mais recente adição ao catálogo da Disney+: DIO. Esta série inovadora promete levar os espectadores a uma jornada extraordinária, repleta de personagens cativantes e tramas envolventes. Com a qualidade cinematográfica pela qual a Disney é conhecida, DIO oferece uma experiência visual deslumbrante, acompanhada de roteiros inteligentes que mantêm os espectadores ávidos por mais a cada episódio. Prepare-se para se emocionar, rir e se surpreender enquanto DIO nos leva a um universo fascinante e inexplorado, disponível agora no Disney+.",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "Wakanda se prepara para novas emoções com o lançamento do próximo filme do Pantera Negra. A trama promete levar os fãs a uma jornada épica, repleta de ação e reviravoltas. Esteja pronto para mais um capítulo inesquecível no universo Marvel.",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "O aguardado novo filme dos X-Men está oficialmente em pós-produção, anunciou o diretor. Com uma narrativa promissora e efeitos visuais revolucionários, a próxima aventura dos mutantes promete surpreender os fãs. Prepare-se para uma experiência cinematográfica extraordinária com o retorno emocionante dos X-Men.",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ]
